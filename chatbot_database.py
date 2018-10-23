02895b24f2975677375c49291571293f68a59dc4
Vishwas Abhyankar <vishwas.abhyankar@default-centos-73.vagrantup.com>
2018-10-23 11:18:39 +0900

    new py file added to create training data

--- chatbot_database.py
+++ chatbot_database.py
@@ -1,9 +1,12 @@
 import sqlite3
 import json
 from datetime import datetime
+import time
 
-timeframe = '2018-09'
+timeframe = '2006-04'
 sql_transaction = []
+start_row = 0
+cleanup = 1000000
 
 connection = sqlite3.connect('{}.db'.format(timeframe))
 c = connection.cursor()
@@ -50,9 +53,9 @@ def sql_insert_no_parent(commentid,parentid,comment,subreddit,time,score):
         print('s0 insertion',str(e))
 
 def acceptable(data):
-    if len(data.split(' ')) > 50 or len(data) < 1:
+    if len(data.split(' ')) > 1000 or len(data) < 1:
         return False
-    elif len(data) > 1000:
+    elif len(data) > 32000:
         return False
     elif data == '[deleted]':
         return False
@@ -90,31 +93,51 @@ if __name__ == '__main__':
     row_counter = 0
     paired_rows = 0
 
-    with open('J:/chatdata/reddit_data/{}/RC_{}'.format(timeframe.split('-')[0],timeframe), buffering=1000) as f:
+    #with open('J:/chatdata/reddit_data/{}/RC_{}'.format(timeframe.split('-')[0],timeframe), buffering=1000) as f:
+    with open('./data/RC_{}'.format(timeframe), buffering=1000) as f:
         for row in f:
+            #print(row)
+            #time.sleep(555)
             row_counter += 1
-            row = json.loads(row)
-            parent_id = row['parent_id']
-            body = format_data(row['body'])
-            created_utc = row['created_utc']
-            score = row['score']
-            comment_id = row['name']
-            subreddit = row['subreddit']
-            parent_data = find_parent(parent_id)
-            if score >= 2:
-                existing_comment_score = find_existing_score(parent_id)
-                if existing_comment_score:
-                    if score > existing_comment_score:
+
+            if row_counter > start_row:
+                try:
+                    row = json.loads(row)
+                    parent_id = row['parent_id'].split('_')[1]
+                    body = format_data(row['body'])
+                    created_utc = row['created_utc']
+                    score = row['score']
+                    
+                    comment_id = row['id']
+                    
+                    subreddit = row['subreddit']
+                    parent_data = find_parent(parent_id)
+                    
+                    existing_comment_score = find_existing_score(parent_id)
+                    if existing_comment_score:
+                        if score > existing_comment_score:
+                            if acceptable(body):
+                                sql_insert_replace_comment(comment_id,parent_id,parent_data,body,subreddit,created_utc,score)
+                                
+                    else:
                         if acceptable(body):
-                            sql_insert_replace_comment(comment_id,parent_id,parent_data,body,subreddit,created_utc,score)
-                            
-                else:
-                    if acceptable(body):
-                        if parent_data:
-                            sql_insert_has_parent(comment_id,parent_id,parent_data,body,subreddit,created_utc,score)
-                            paired_rows += 1
-                        else:
-                            sql_insert_no_parent(comment_id,parent_id,body,subreddit,created_utc,score)
+                            if parent_data:
+                                if score >= 2:
+                                    sql_insert_has_parent(comment_id,parent_id,parent_data,body,subreddit,created_utc,score)
+                                    paired_rows += 1
+                            else:
+                                sql_insert_no_parent(comment_id,parent_id,body,subreddit,created_utc,score)
+                except Exception as e:
+                    print(str(e))
                             
             if row_counter % 100000 == 0:
                 print('Total Rows Read: {}, Paired Rows: {}, Time: {}'.format(row_counter, paired_rows, str(datetime.now())))
+
+            if row_counter > start_row:
+                if row_counter % cleanup == 0:
+                    print("Cleanin up!")
+                    sql = "DELETE FROM parent_reply WHERE parent IS NULL"
+                    c.execute(sql)
+                    connection.commit()
+                    c.execute("VACUUM")
+                    connection.commit()
