b2d63ed13c7889b87cefc72ae099b7e001120632
Vishwas Abhyankar <vishwas.abhyankar@default-centos-73.vagrantup.com>
2018-10-23 10:27:59 +0900

    added functions to filter unwanted comments and getting score of the comment

--- chatbot_database.py
+++ chatbot_database.py
@@ -13,7 +13,19 @@ def create_table():
 
 def format_data(data):
     data = data.replace('\n',' newlinechar ').replace('\r',' newlinechar ').replace('"',"'")
-    return data 
+    return data
+
+def acceptable(data):
+    if len(data.split(' ')) > 50 or len(data) < 1:
+        return False
+    elif len(data) > 1000:
+        return False
+    elif data == '[deleted]':
+        return False
+    elif data == '[removed]':
+        return False
+    else:
+        return True
 
 def find_parent(pid):
     try:
@@ -27,6 +39,18 @@ def find_parent(pid):
         #print(str(e))
         return False
 
+def find_existing_score(pid):
+    try:
+        sql = "SELECT score FROM parent_reply WHERE parent_id = '{}' LIMIT 1".format(pid)
+        c.execute(sql)
+        result = c.fetchone()
+        if result != None:
+            return result[0]
+        else: return False
+    except Exception as e:
+        #print(str(e))
+        return False
+    
 if __name__ == '__main__':
     create_table()
     row_counter = 0
@@ -43,5 +67,5 @@ if __name__ == '__main__':
             comment_id = row['name']
             subreddit = row['subreddit']
             parent_data = find_parent(parent_id)
-
-  
+            if score >= 2:
+                existing_comment_score = find_existing_score(parent_id)
